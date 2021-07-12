c09a79f9f096f3c1434be7b84a44abbec7c51954
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-11 23:50:02 -0600

    fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let clickUpgrades = {
     apple: {
         price: 5,
         quantity: 0,
-        multiplier: 2
+        multiplier: 1.5
     },
     orange: {
         price: 10,
@@ -20,7 +20,7 @@ let automaticUpgrades = {
     dog: {
         price: 15,
         quantity: 0,
-        multiplier: 4
+        multiplier: 3.5
     },
     cat: {
         price: 25,
@@ -33,123 +33,71 @@ let automaticUpgrades = {
 function update() {
     document.getElementById('cheese').innerText = cheese
     document.getElementById('che').innerText = cheese
-    // document.getElementById('app').innerText = click
+    displayInventory();
 }
 function buyClickUpgrade(term) {
     let cost = clickUpgrades[term].price
     let qty = clickUpgrades[term].quantity
-    // let bonus = clickUpgrades[term].multiplier
+
     let multi = clickUpgrades[term].multiplier
 
     if (cheese >= cost) {
         qty++
         cheese -= cost
         cost += Math.abs(Math.round(multi))
-
-
-        if (qty >= 5) {
-            cheese += 1;
-        }
     }
 
 
-    clickUpgrades[term].price = cost;
-    clickUpgrades[term].quantity = qty;
-    clickUpgrades[term].multiplier = multi;
-    //   debugger;
-    // document.getElementById("apple").innerText = clickUpgrades[term].quantity;
-    // update()
+    clickUpgrades[term].price = Math.floor(Math.round(cost));
+    clickUpgrades[term].quantity = Math.floor(Math.round(qty));
+    clickUpgrades[term].multiplier = Math.floor(Math.round(multi));
 
 
     if (term == 'apple') {
-        document.getElementById('apple').innerText = cost
+        document.getElementById('apple').innerText = "🍎 " + clickUpgrades[term].price + " points";
+        // cost//
         document.getElementById("app").innerHTML = clickUpgrades[term].quantity;
     }
 
 
     if (term == 'orange') {
-        document.getElementById('orange').innerText = cost
+        document.getElementById('orange').innerText = "🍊 " + clickUpgrades[term].price + " points"
         document.getElementById("org").innerHTML = clickUpgrades[term].quantity;
 
     }
-    // document.getElementById("app").innerHTML = clickUpgrades[term].quantity;
-
     update()
 
-    displayInventory()
+    // displayInventory()
 }
-// function buyAutomaticUpgrade(par) {
-//     let cost = automaticUpgrades[par].price
-//     let amount = automaticUpgrades[par].quantity
-//     let bonus = Math.floor(automaticUpgrades[par].multiplier)
-//     setInterval((automaticClick) => {
-//         if (cheese >= cost) {
-//             amount++
-//             click += bonus
-//             cheese += cost
-//         }
-//     }, 3000);
-
-//     update()
-//     displayInventory()
-// }
-
 function buyAutomaticUpgrade(par) {
     let cost = automaticUpgrades[par].price
     let qty = automaticUpgrades[par].quantity
-    // let bonus = buyAutomaticUpgrade[term].multiplier
+
     let multi = automaticUpgrades[par].multiplier
 
     if (cheese >= cost) {
         qty++
         cheese -= cost
         cost += Math.abs(Math.round(multi))
-
-
-        if (qty >= 5) {
-            cheese += 1;
-        }
     }
-
 
     automaticUpgrades[par].price = cost;
     automaticUpgrades[par].quantity = qty;
     automaticUpgrades[par].multiplier = multi;
-    //   debugger;
-    // document.getElementById("apple").innerText = clickUpgrades[par].quantity;
-    // update()
-
 
     if (par == 'dog') {
-        document.getElementById('dog').innerText = cost
+        document.getElementById('dog').innerText = "🐶 " + cost + " points"
         document.getElementById("do").innerHTML = automaticUpgrades[par].quantity;
     }
 
 
     if (par == 'cat') {
-        document.getElementById('cat').innerText = cost
+        document.getElementById('cat').innerText = "😺 " + cost + " points"
         document.getElementById("ca").innerHTML = automaticUpgrades[par].quantity;
-
     }
-    // document.getElementById("app").innerHTML = automaticUpgrades[term].quantity;
-
     update()
-
     displayInventory()
 
-
-
-
-    // let cost = automaticUpgrades[par].price
-    // let amount = automaticUpgrades[par].quantity
-    // let bonus = Math.floor(automaticUpgrades[par].multiplier)
-    // if (cheese >= cost) {
-    //     amount++
-    //     click += bonus
-    //     cheese += cost
-    // }
-    // update()
-    // displayInventory()
 }
 
 function mine(increaseValue) {
@@ -161,40 +109,43 @@ function mine(increaseValue) {
     update()
 }
 
-// function buyAutoSetInt(btnval) {
-//     let intId;
+function buyAutoSetInt(btnVal) {
+    let intId;
 
-//     console.log(btnval)
-//     intId = setInterval(incrementCheese, 1000)
-// }
+    console.log(btnVal)
+    buyAutomaticUpgrade(btnVal);
+    intId = setInterval(mine, 1000);
+
+}
 
 function displayInventory() {
     if (clickUpgrades.apple) {
         cheeseCheck = clickUpgrades.apple.price
-        //     if (cheese <= cheeseCheck)
-        //          document.getElementById('apple').disabled = true
-        //     else
-        //         document.getElementById('apple').disabled = false
+        if (cheese >= cheeseCheck)
+            document.getElementById('apple').disabled = false
+        else
+            document.getElementById('apple').disabled = true
     }
     if (clickUpgrades.orange) {
         cheeseCheck = clickUpgrades.orange.price
-        // if (cheese <= cheeseCheck)
-        // document.getElementById('orange').disabled = true
-        // else
-        // document.getElementById('orange').disabled = false
+        if (cheese >= cheeseCheck)
+            document.getElementById('orange').disabled = false
+        else
+            document.getElementById('orange').disabled = true
     }
     if (automaticUpgrades.dog) {
         cheeseCheck = automaticUpgrades.dog.price
-        // if (cheese <= cheeseCheck)
-        //     document.getElementById('dog').disabled = true
-        // else
-        //     document.getElementById('dog').disabled = false
+        if (cheese >= cheeseCheck)
+            document.getElementById('dog').disabled = false
+        else
+            document.getElementById('dog').disabled = true
     }
     if (automaticUpgrades.cat) {
         cheeseCheck = automaticUpgrades.cat.price
-        // if (cheese <= cheeseCheck)
-        //     document.getElementById('cat').disabled = true
-        // else
-        //     document.getElementById('cat').disabled = false
+        if (cheese >= cheeseCheck)
+            document.getElementById('cat').disabled = false
+        else
+            document.getElementById('cat').disabled = true
     }
 }
+update();
--- backUp.js
+++ backUp.js
@@ -5,57 +5,147 @@ let autoClick = 0
 
 let clickUpgrades = {
     apple: {
-        price: 10,
+        price: 5,
         quantity: 0,
-        multiplier: 1
+        multiplier: 1.5
     },
     orange: {
-        price: 20,
+        price: 10,
         quantity: 0,
-        multiplier: 10
+        multiplier: 3
     }
 };
 
 let automaticUpgrades = {
     dog: {
-        price: 50,
+        price: 15,
         quantity: 0,
-        multiplier: 15
+        multiplier: 3.5
     },
     cat: {
-        price: 100,
+        price: 25,
         quantity: 0,
-        multiplier: 20
+        multiplier: 5
     }
-}
-function mine() {
-    // alert("The number of your cheese is :" + cheese)
-    cheese += click
-    update()
-}
-function update() {
-    document.getElementById('cheese').innerText = cheese
 }
 
-function buyClickUpgrade(item) {
-    let cost = clickUpgrades[item].price
-    let amount = clickUpgrades[item].quantity
-    let bonus = clickUpgrades[item].multiplier
+
+function update() {
+    document.getElementById('cheese').innerText = cheese
+    document.getElementById('che').innerText = cheese
+    displayInventory();
+}
+function buyClickUpgrade(term) {
+    let cost = clickUpgrades[term].price
+    let qty = clickUpgrades[term].quantity
+
+    let multi = clickUpgrades[term].multiplier
+
     if (cheese >= cost) {
-        amount++
-        click += bonus
+        qty++
         cheese -= cost
+        cost += Math.abs(Math.round(multi))
+    }
+
+
+    clickUpgrades[term].price = Math.floor(Math.round(cost));
+    clickUpgrades[term].quantity = Math.floor(Math.round(qty));
+    clickUpgrades[term].multiplier = Math.floor(Math.round(multi));
+
+
+    if (term == 'apple') {
+        document.getElementById('apple').innerText = "🍎 " + clickUpgrades[term].price + " points";
+        // cost//
+        document.getElementById("app").innerHTML = clickUpgrades[term].quantity;
+    }
+
+
+    if (term == 'orange') {
+        document.getElementById('orange').innerText = "🍊 " + clickUpgrades[term].price + " points"
+        document.getElementById("org").innerHTML = clickUpgrades[term].quantity;
+
     }
     update()
+
+    // displayInventory()
 }
-function buyAutomaticUpgrade(item) {
-    let cost = automaticUpgrades[item].price
-    let amount = automaticUpgrades[item].quantity
-    let bonus = automaticUpgrades[item].multiplier
+function buyAutomaticUpgrade(par) {
+    let cost = automaticUpgrades[par].price
+    let qty = automaticUpgrades[par].quantity
+
+    let multi = automaticUpgrades[par].multiplier
+
     if (cheese >= cost) {
-        amount++
-        click += bonus
+        qty++
         cheese -= cost
+        cost += Math.abs(Math.round(multi))
+    }
+
+    automaticUpgrades[par].price = cost;
+    automaticUpgrades[par].quantity = qty;
+    automaticUpgrades[par].multiplier = multi;
+
+    if (par == 'dog') {
+        document.getElementById('dog').innerText = "🐶 " + cost + " points"
+        document.getElementById("do").innerHTML = automaticUpgrades[par].quantity;
+    }
+
+
+    if (par == 'cat') {
+        document.getElementById('cat').innerText = "😺 " + cost + " points"
+        document.getElementById("ca").innerHTML = automaticUpgrades[par].quantity;
     }
     update()
+    displayInventory()
+
 }
+
+function mine(increaseValue) {
+    let IntCheese = 1
+    if (increaseValue) {
+        cheese += increaseValue
+    } else
+        cheese += IntCheese
+    update()
+}
+
+function buyAutoSetInt(btnVal) {
+    let intId;
+
+    console.log(btnVal)
+    buyAutomaticUpgrade(btnVal);
+    intId = setInterval(mine, 1000);
+
+}
+
+function displayInventory() {
+    if (clickUpgrades.apple) {
+        cheeseCheck = clickUpgrades.apple.price
+        if (cheese >= cheeseCheck)
+            document.getElementById('apple').disabled = false
+        else
+            document.getElementById('apple').disabled = true
+    }
+    if (clickUpgrades.orange) {
+        cheeseCheck = clickUpgrades.orange.price
+        if (cheese >= cheeseCheck)
+            document.getElementById('orange').disabled = false
+        else
+            document.getElementById('orange').disabled = true
+    }
+    if (automaticUpgrades.dog) {
+        cheeseCheck = automaticUpgrades.dog.price
+        if (cheese >= cheeseCheck)
+            document.getElementById('dog').disabled = false
+        else
+            document.getElementById('dog').disabled = true
+    }
+    if (automaticUpgrades.cat) {
+        cheeseCheck = automaticUpgrades.cat.price
+        if (cheese >= cheeseCheck)
+            document.getElementById('cat').disabled = false
+        else
+            document.getElementById('cat').disabled = true
+    }
+}
+update();
